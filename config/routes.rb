c750a6cc18f0fb5ec14d75d3b9eb83a38e1e19bf
GUILLEUX VINCENT <thecinematicorchestra@yahoo.fr>
2014-02-25 15:44:28 +0100

    contact form Rails way

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 LearnRails::Application.routes.draw do
-	post 'contact', to: 'contacts#process_form'
+	resources :contacts, only: [:new, :create]
 	root to: 'visitors#new' #Any request to the application root (http://localhost:3000/) will be directed to the VisitorsController new action.
 end
