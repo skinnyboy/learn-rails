93221bfb900e23ae5dc7a0171434b1e452fcca7f
GUILLEUX VINCENT <thecinematicorchestra@yahoo.fr>
2014-02-26 14:49:01 +0100

    mailing list MailChimp

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 LearnRails::Application.routes.draw do
 	resources :contacts, only: [:new, :create]
+	resources :visitors, only: [:new, :create]
 	root to: 'visitors#new' #Any request to the application root (http://localhost:3000/) will be directed to the VisitorsController new action.
 end
